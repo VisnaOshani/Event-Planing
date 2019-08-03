c09e3dcd822349dd7fa84cb25d5ee4086e45382f
visnaOshani <jayasinghe548@gmail.com>
2019-08-03 17:23:41 +0530

    my part

---
+++ app/src/main/java/com/example/first_activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.first_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void login (View view){
+
+     Intent intent = new Intent(MainActivity.this,Second_Activity.class);
+    //Intent intent = new Intent(this,MainActivity.class);
+    startActivity(intent);
+
+
+    }
+}
